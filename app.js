bfa77700eb9397aa245e20a52b1fee56cff94c92
lleellee0 <lleellee013@gmail.com>
2019-06-22 22:54:55 +0900

    calcNdaySuccessRate까지 구현 완료

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const history = require('./history');
 const statistics = require('./statistics');
 
+const target_profit = 3;
+
 const main = async () => {
     let history_obj = await history.getHistory('43430', '1361192945', Date.now());
     let history_arr = history.transferObjToArr(history_obj);
@@ -8,7 +10,25 @@ const main = async () => {
     history_arr = history.calcDifferenceRate(history_arr);
 
     history_arr = statistics.calcCeilDifferenceRate(history_arr);
+    
+    let issue_obj = {};
+    
+    issue_obj.name = 'SK Hynix Inc';
+    issue_obj.price = history_arr[history_arr.length-1].close_price;
+    issue_obj.ma25 = history_arr[history_arr.length-1].ma25;
+    issue_obj.difference_rate = history_arr[history_arr.length-1].difference_rate;
+    issue_obj.ceiled_difference_rate = history_arr[history_arr.length-1].ceiled_difference_rate;
+    issue_obj.in_3_days = statistics.calcNdaySuccessRate(history_arr, 3, target_profit);
+    // issue_obj.in_3_days_profit_rate = calcNdayProfitRate(history_arr, 3);
+    issue_obj.in_5_days = statistics.calcNdaySuccessRate(history_arr, 5, target_profit);
+    // issue_obj.in_5_days_profit_rate = calcNdayProfitRate(history_arr, 5);
+    issue_obj.in_7_days = statistics.calcNdaySuccessRate(history_arr, 7, target_profit);
+    // issue_obj.in_7_days_profit_rate = calcNdayProfitRate(history_arr, 7);
+
+    console.log(issue_obj);
+
     console.log(history_arr[history_arr.length-1]);
+    console.log('history_arr 길이 : ' + history_arr.length);
 }
 
 main();
--- statistics.js
+++ statistics.js
@@ -5,6 +5,37 @@ const calcCeilDifferenceRate = (history_arr) => {
     return history_arr;
 }
 
+const calcNdaySuccessRate = (history_arr, days, target_profit) => {
+    let current_history_obj = history_arr[history_arr.length-1];
+    let success_count = 0;
+    let fail_count = 0;
+    for(let i = 24; i < history_arr.length - days; i++) {
+        let success_flag = false;
+        // history_arr을 순회하면서 같은 괴리율을 가지는 것으로 비교를 한다.
+        if(history_arr[i].ceiled_difference_rate === current_history_obj.ceiled_difference_rate) {
+            for(let j = 1; j <= days; j++) {
+                if(history_arr[i+j].high_price > history_arr[i].close_price * (100 + target_profit) / 100) {
+                    success_flag = true;
+                    j = 999999999;  // 내부 for문 종료
+                }
+            }
+            if(success_flag === true)   success_count++;
+            else    fail_count++;
+        }
+    }
+
+    let obj = {};
+    obj.success_count = success_count;
+    obj.fail_count = fail_count;
+    obj.success_rate = success_count / (success_count + fail_count) * 100;
+    return obj;
+}
+
+const calcNdayProfitRate = (history_arr, days) => {
+
+}
+
 module.exports = {
-    calcCeilDifferenceRate: calcCeilDifferenceRate
+    calcCeilDifferenceRate: calcCeilDifferenceRate,
+    calcNdaySuccessRate: calcNdaySuccessRate
 }
